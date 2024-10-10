4a58617437edb0bab7c534a7462a1bbb355c81f8
Youssef-elmohamadi <youssefelmohammadi24@gmail.com>
2024-10-11 00:07:52 +0300

    edit2

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Route,Switch } from "react-router-dom";
+import { BrowserRouter, Route,Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const basename = window.location.hostname === "localhost" ? "" : "/store";
 
 root.render(
   <BrowserRouter basename="{basename}" >
-    <Switch>
+    <Routes>
     <App />
-    </Switch>
+    </Routes>
   </BrowserRouter>
 );
